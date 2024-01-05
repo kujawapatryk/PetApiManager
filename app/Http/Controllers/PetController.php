b7a3d17b1391842194880bb85f414e6af70b1b97
kujawapatryk <homik363@wp.pl>
2024-01-05 16:04:03 +0100

    Fetching Pet data and passing it to the pets.index blade.

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\PetRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class PetController extends Controller
 {
-    public function index(){
+    protected PetRepository $apiPet;
+    public function __construct(PetRepository $apiPet)
+    {
+        $this->apiPet = $apiPet;
+    }
 
+    public function index(){
+        $data = $this->apiPet->getAll();
+        return view('pets.index', ['pets' => $data]);
     }
 
     public function create(){
--- app/Repositories/PetRepository.php
+++ app/Repositories/PetRepository.php
@@ -2,6 +2,20 @@
 
 namespace App\Repositories;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
+
 class PetRepository{
+
+    public function getAll(): JsonResponse{
+        $response = Http::get('https://petstore.swagger.io/v2/pet/findByStatus?status=available&status=pending&status=sold');
+        return $response->json();
+
+    }
+
+    public function getOne($id): JsonResponse{
+        $response = Http::get('https://petstore.swagger.io/v2/pet/'.$id);
+        return $response->json();
+    }
 
 }
