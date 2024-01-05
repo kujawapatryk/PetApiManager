278b42334a32f437f7e7244a64acd100d02aaa0e
kujawapatryk <homik363@wp.pl>
2024-01-05 19:29:57 +0100

    Changing the returned data to an array and adding a create method.

--- app/Repositories/PetRepository.php
+++ app/Repositories/PetRepository.php
@@ -7,14 +7,20 @@ use Illuminate\Support\Facades\Http;
 
 class PetRepository{
 
-    public function getAll(): JsonResponse{
+    public function getAll(): array{
         $response = Http::get('https://petstore.swagger.io/v2/pet/findByStatus?status=available&status=pending&status=sold');
         return $response->json();
 
     }
 
-    public function getOne($id): JsonResponse{
+    public function getOne($id): array{
         $response = Http::get('https://petstore.swagger.io/v2/pet/'.$id);
+        return $response->json();
+    }
+
+    public function create(array $data): array{
+
+        $response = Http::post('https://petstore.swagger.io/v2/pet',$data);
         return $response->json();
     }
 
