b6693858caa0394f2beca75e3f518fe255964be4
kujawapatryk <homik363@wp.pl>
2024-01-05 14:26:42 +0100

    Initial setup of the controller and repository for pet.

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class PetController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+    public function destroy($id){
+
+    }
+
+
+}
