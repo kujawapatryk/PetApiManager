f9ddfa6ef930d2f8be69c5836b7c1e4dc000aac1
kujawapatryk <homik363@wp.pl>
2024-01-06 22:45:27 +0100

    Adding a confirmation button.

--- resources/views/pets/edit.blade.php
+++ resources/views/pets/edit.blade.php
@@ -56,6 +56,9 @@
         </div>
 
         <div class="flex items-center justify-between">
+            <a href="{{ route('pets.index') }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Cofnij
+            </a>
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 Zaktualizuj
             </button>
